82233358188bd29451597fcb90bada4624a5f2a7
root <root@ubuntu.(none)>
2011-10-02 11:27:04 -0400

    this is my first commit

---
+++ forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+class GradeForm(forms.Form):
+	grade1=forms.CharField(max_length=1)
+	grade2=forms.CharField(max_length=1)
+	grade3=forms.CharField(max_length=1)
+	grade4=forms.CharField(max_length=1)
+	grade5=forms.CharField(max_length=1)
+	grade6=forms.CharField(max_length=1)
+	grade7=forms.CharField(max_length=1)
+	grade8=forms.CharField(max_length=1)
+	grade9=forms.CharField(max_length=1)
+
+class GradeForm1(forms.Form):
+	grade1=forms.CharField(max_length=1)
+	grade2=forms.CharField(max_length=1)
+	grade3=forms.CharField(max_length=1)
+	grade4=forms.CharField(max_length=1)
+	grade5=forms.CharField(max_length=1)
+	grade6=forms.CharField(max_length=1)
+	grade7=forms.CharField(max_length=1)
+	grade8=forms.CharField(max_length=1)
+
+
+	
---
+++ views.py
@@ -0,0 +1,77 @@
+from django.http import HttpResponse
+import datetime
+from django.shortcuts import render_to_response
+
+def ip(request):
+	return render_to_response('gpa.html')
+
+def calc(request):
+	global final
+	final=[]	
+	t="hi all"
+	global lis
+	lis=[{'sem':'1','dep':'ece','sub':'english','credit':'4'},\
+             {'sem':'1','dep':'ece','sub':'maths','credit':'4'},\
+             {'sem':'1','dep':'ece','sub':'physics','credit':'3'},\
+	     {'sem':'1','dep':'ece','sub':'chemistry','credit':'3'},\
+	     {'sem':'1','dep':'ece','sub':'Engneering-graphics','credit':'5'},\
+	     {'sem':'1','dep':'ece','sub':'FOC','credit':'3'},\
+	     {'sem':'1','dep':'ece','sub':'computer-lab','credit':'2'},\
+	     {'sem':'1','dep':'ece','sub':'engneering-lab','credit':'2'},\
+	     {'sem':'2','dep':'ece','sub':'english2','credit':'4'},\
+	     {'sem':'2','dep':'ece','sub':'maths2','credit':'4'},\
+	     {'sem':'2','dep':'ece','sub':'physics2','credit':'3'},\
+	     {'sem':'2','dep':'ece','sub':'chemistry2','credit':'3'},\
+	     {'sem':'2','dep':'ece','sub':'EDC','credit':'4'},\
+	     {'sem':'2','dep':'ece','sub':'BCM','credit':'4'},\
+	     {'sem':'2','dep':'ece','sub':'EDC-lab','credit':'2'},\
+	     {'sem':'2','dep':'ece','sub':'physics-lab','credit':'2'},\
+	     {'sem':'2','dep':'ece','sub':'chemistry-lab','credit':'2'},\
+	     {'sem':'1','dep':'it','sub':'english','credit':'4'},\
+	     {'sem':'1','dep':'it','sub':'maths','credit':'4'},\
+	     {'sem':'1','dep':'it','sub':'physics','credit':'3'},\
+	     {'sem':'1','dep':'it','sub':'chemistry','credit':'3'},\
+	     {'sem':'1','dep':'it','sub':'engneering-drawing','credit':'5'},\
+	     {'sem':'1','dep':'it','sub':'FOC','credit':'3'},\
+	     {'sem':'1','dep':'it','sub':'engneering-lab','credit':'2'},\
+	     {'sem':'1','dep':'it','sub':'computer-lab','credit':'2'},\
+	     {'sem':'2','dep':'it','sub':'english2','credit':'4'},\
+	     {'sem':'2','dep':'it','sub':'maths2','credit':'4'},\
+	     {'sem':'2','dep':'it','sub':'physics2','credit':'3'},\
+	     {'sem':'2','dep':'it','sub':'chemistry2','credit':'3'},\
+	     {'sem':'2','dep':'it','sub':'EDC','credit':'4'},\
+	     {'sem':'2','dep':'it','sub':'bcm','credit':'4'},\
+	     {'sem':'2','dep':'it','sub':'phy-lab','credit':'2'},\
+	     {'sem':'2','dep':'it','sub':'chem-lab','credit':'2'},\
+	     {'sem':'2','dep':'it','sub':'edc-lab','credit':'2'}]
+	if 'sem' and 'dep' in request.GET:
+		sem=request.GET['sem']
+		dep=request.GET['dep']
+		for l in lis:
+			if l['sem']==sem and l['dep']==dep:
+				final.append(l)
+	return render_to_response('table.html',{'list':final})
+
+
+def value(request): 
+	j=0
+	i=0
+	lis=['s','a','b','c','d','e','u']
+	dic={'s':10,'a':9,'b':8,'c':7,'d':6,'e':5,'u':0}
+	for d in final:
+		a=d['sub']
+		c=d['credit']
+		c=int(c)
+		s=c+j
+		j=s
+		if a in request.GET:
+			grade=request.GET[a]
+			if grade in lis:
+			
+				g=dic[grade]
+				p=c*g
+				t=p+i
+				i=t
+	gpa=t/s
+				
+	return render_to_response('final.html',{'lst':gpa})	
